9f9acca76a3404b250da0a4976c0330668c5f047
Martin Ohmann <martin.ohmann@lesara.de>
2018-09-18 09:09:27 +0200

    [master] add license header

--- tests/Formatter/MappingFormatterTest.php
+++ tests/Formatter/MappingFormatterTest.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of the elasticsearch-synonyms package.
+ *
+ * (c) Martin Ohmann <martin@mohmann.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace mohmann\ElasticsearchSynonyms\Tests\Formatter;
 
